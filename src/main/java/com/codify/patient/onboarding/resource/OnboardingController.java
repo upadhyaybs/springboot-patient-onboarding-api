66c0b90fcf2a9f3c5b371d3d1c1659ba07827e3d
bupadhyay <upadhyaybs@live.com>
2022-10-03 22:25:06 -0500

    correlationId

--- src/main/java/com/codify/patient/onboarding/resource/OnboardingController.java
+++ src/main/java/com/codify/patient/onboarding/resource/OnboardingController.java
@@ -3,6 +3,7 @@ package com.codify.patient.onboarding.resource;
 import com.codify.patient.onboarding.domain.Request;
 import com.codify.patient.onboarding.domain.Response;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 import com.codify.patient.onboarding.service.IOnboardingService;
@@ -10,7 +11,7 @@ import com.codify.patient.onboarding.service.IOnboardingService;
 import reactor.core.publisher.Mono;
 
 
-
+@Slf4j
 @RestController
 @RequestMapping("/onboarding")
 @Tag(name = "Onboaring APIs", description = "Onboaring APIs for demo purpose")
@@ -24,11 +25,13 @@ public class OnboardingController {
 
 	@PostMapping("/assignhospital/{zip}")
 	public Mono<Response> assignHospitalToPatient(@RequestBody Request request, @PathVariable("zip") String zip) {
+		log.info("Request : " +request + " | zip :" +zip);
 		return Mono.just(Response.builder().correlationId(request.getCorrelationId()).businessKey(request.getBusinessKey()).id(request.getId()).hospital(service.getParticipatingHospital(zip)).build());
 	}
 
 	@PostMapping("/assigndoctor/{condition}")
 	public Mono<Response> assignDoctorToPatient(@RequestBody Request request,@PathVariable("condition") String condition) {
+		log.info("Request : " +request + " | condition :" +condition);
 		return Mono.just(Response.builder().correlationId(request.getCorrelationId()).businessKey(request.getBusinessKey()).id(request.getId()).doctor(service.getParticipatingDoctor(condition)).build());
 	}
 
